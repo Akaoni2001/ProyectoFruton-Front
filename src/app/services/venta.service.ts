7d56ee847e0f21e20941c1f947f4457814eb3064
lauu1511 <lauracarranzamarin@gmail.com>
2024-09-29 17:40:22 -0500

    primero

--- src/app/services/venta.service.ts
+++ src/app/services/venta.service.ts
@@ -8,7 +8,7 @@ import { Venta } from '../models/venta';
 })
 export class VentaService {
 
-  url = 'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/ventas'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/ventas' o 'http://localhost:4000/api/ventas'
+  url = 'http://localhost:4000/api/ventas'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/ventas' o 'http://localhost:4000/api/ventas'
 
   constructor(private http: HttpClient) { }
 
