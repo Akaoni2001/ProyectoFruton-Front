fcbcb28a5f05a770c6c0bff399cf84194d08a45c
Akaoni <91914277+Akaoni2001@users.noreply.github.com>
2024-12-03 08:07:19 +0000

    aaa

--- src/app/services/reporte.service.ts
+++ src/app/services/reporte.service.ts
@@ -8,7 +8,7 @@ import { Reportes } from '../models/reportes';
 })
 export class ReporteService {
 
-  url = 'http://localhost:4000/api/reportes'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/categorias' o 'http://localhost:4000/api/categorias'
+  url = 'https://organic-space-halibut-6777q564w55hrj4v-4000.app.github.dev/api/reportes'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/categorias' o 'http://localhost:4000/api/categorias'
 
   constructor(private http:HttpClient) { }
   
--- src/app/services/venta.service.ts
+++ src/app/services/venta.service.ts
@@ -8,7 +8,7 @@ import { Venta } from '../models/venta';
 })
 export class VentaService {
 
-  url = 'http://localhost:4000/api/ventas'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/ventas' o 'http://localhost:4000/api/ventas'
+  url = 'https://organic-space-halibut-6777q564w55hrj4v-4000.app.github.dev/api/ventas'; //'https://solid-enigma-49995v4wrrph7vx7-4000.app.github.dev/api/ventas' o 'http://localhost:4000/api/ventas'
 
   constructor(private http: HttpClient) { }
 
