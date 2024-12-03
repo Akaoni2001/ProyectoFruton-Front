e8238448e5f07b21dc7a01fb25fe2d007990b60b
Alezzandromv <ale722sandromv35forma@gmail.com>
2024-12-03 11:20:53 -0500

    a

--- src/app/models/producto.ts
+++ src/app/models/producto.ts
@@ -1,20 +1,20 @@
-export class Producto {
-    _id?:string;
-    nombre: string;
-    descripcion:string;
-    categoria: string;
-    precio:number;
-    stock:number;
-    imagen:string;
-    estado:boolean;
+    export class Producto {
+        _id?:string;
+        nombre: string;
+        descripcion:string;
+        categoria: string;
+        precio:number;
+        stock:number;
+        imagen:string;
+        estado:boolean;
 
-    constructor(nombre:string, descripcion:string, categoria:string, precio:number, stock:number, imagen:string, estado:boolean){
-        this.nombre= nombre;
-        this.descripcion=descripcion;
-        this.categoria=categoria;
-        this.precio=precio;
-        this.stock=stock;
-        this.imagen=imagen;
-        this.estado=estado;
+        constructor(nombre:string, descripcion:string, categoria:string, precio:number, stock:number, imagen:string, estado:boolean){
+            this.nombre= nombre;
+            this.descripcion=descripcion;
+            this.categoria=categoria;
+            this.precio=precio;
+            this.stock=stock;
+            this.imagen=imagen;
+            this.estado=estado;
+        }
     }
-}
